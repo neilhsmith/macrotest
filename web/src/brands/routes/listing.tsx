7668a1b91f8d83c0891a869dfcf6f518fc796e3c
Neil Smith <me+github@neilsmith.fyi>
2023-12-27 19:58:05 -0600

    setup navigate to handle the table pagination

--- web/src/brands/routes/listing.tsx
+++ web/src/brands/routes/listing.tsx
@@ -1,6 +1,7 @@
-import { Route } from "@tanstack/react-router"
+import { Route, useNavigate } from "@tanstack/react-router"
 import { paginationParamsSchema } from "@/api/pagination"
 import { brandsRoute } from "../router"
+import { useCallback } from "react"
 
 export const brandsListingRoute = new Route({
   getParentRoute: () => brandsRoute,
@@ -11,9 +12,38 @@ export const brandsListingRoute = new Route({
 
 function BrandsIndexRouteComponent() {
   const { page, pageSize } = brandsListingRoute.useSearch()
+  const navigate = useNavigate({ from: "/brands" })
+
+  const changePage = useCallback(
+    (page: number) => {
+      navigate({
+        to: "/brands",
+        search: {
+          page,
+          pageSize,
+        },
+      })
+    },
+    [navigate, pageSize]
+  )
+
+  const changePageSize = useCallback(
+    (pageSize: number) => {
+      navigate({
+        to: "/brands",
+        search: {
+          page,
+          pageSize,
+        },
+      })
+    },
+    [navigate, page]
+  )
 
   return (
     <div>
+      <button onClick={() => changePage(page + 1)}>asdf</button>
+      <button onClick={() => changePageSize(pageSize + 1)}>fdfdf</button>
       brands index route component - {page}, {pageSize}
     </div>
   )
